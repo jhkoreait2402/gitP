020f681d4327de8a946080976df877b899cc5e85
admin <admin@DESKTOP-8VOVB0M>
2024-02-20 15:01:26 +0900

    hi2

---
+++ gitP/src/main/resources/dto/ComputerDTO.java
@@ -0,0 +1,40 @@
+package dto;
+
+public class ComputerDTO {
+
+	float cpu;
+	int ram, power;
+	String gpu, mainBoard;
+	public float getCpu() {
+		return cpu;
+	}
+	public void setCpu(float cpu) {
+		this.cpu = cpu;
+	}
+	public int getRam() {
+		return ram;
+	}
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public String getGpu() {
+		return gpu;
+	}
+	public void setGpu(String gpu) {
+		this.gpu = gpu;
+	}
+	public String getMainBoard() {
+		return mainBoard;
+	}
+	public void setMainBoard(String mainBoard) {
+		this.mainBoard = mainBoard;
+	}
+	
+	
+}
